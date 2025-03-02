b24ff7a428ffffb4035b86cf9584a7e8176e5fd7
ogabek <ogbkiy@gmail.com>
2025-03-02 16:56:35 +0500

    course validation added

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,1 +1,55 @@
-export class CreateCourseDto {}
+import { IsDate, IsIn, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateCourseDto {
+    @IsString()
+    @IsNotEmpty()
+    title: string
+
+    @IsString()
+    @IsOptional()
+    description: string
+
+    @IsNumber()
+    @IsNotEmpty()
+    teacher_id: number
+
+    @IsNumber()
+    @IsNotEmpty()
+    price: number
+
+    @IsNumber()
+    @IsNotEmpty()
+    category_id: number
+
+    @IsDate()
+    @IsOptional()
+    start_date: Date
+
+    @IsDate()
+    @IsOptional()
+    end_date: Date
+
+    @IsString()
+    @IsOptional()
+    @IsIn(['pending','active','completed'],{message:  'status must be either pending, active, or completed'})
+    status: string
+
+    @IsNumber()
+    @IsNotEmpty()
+    room_id: number
+
+    @IsString()
+    @IsNotEmpty()
+    start_time: string
+
+    @IsString()
+    @IsNotEmpty()
+    end_time: string
+
+    @IsString()
+    @IsNotEmpty()
+    @IsIn(['even','odd'],{message: 'days must be either even or odd'})
+    days: string
+
+    
+}
